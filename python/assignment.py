e71a10229ca6fe42e460dae85586ed66faa75a6e
haward0519 <110471342+haward0519@users.noreply.github.com>
2023-03-27 16:39:40 +0900

    Add files via upload

---
+++ python/assignment.py
@@ -0,0 +1,24 @@
+print(-1)
+print(0)
+print(1)
+print(1.1)
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('2*1', 2-1)
+
+print('hello world'*1000)
+print(len('hello world'*1000))
+
+
+grades = [2,1,4]
+print("min(grades)", min(grades))
+import statistics
+print("statistics.mean(grades)", statistics.mean(grades))
+
+name = 'sory'
+message = 'hi, '+name+' .... bye, '+name+'.'
+print(message)
+
+name = input('name: ')
+message = 'hi, '+name+' .... bye, '+name+'.'
+print(message)
---
+++ python/problem.py
@@ -0,0 +1,41 @@
+# d = "The Curious Case of Benjamin Button"
+
+# len(d.split())
+
+a = input()
+
+# print(a, type(a), a[::-1])
+
+# print(reversedInput)
+if  a[:Zz]== a : 
+    print("yes")
+else:
+    print("no")
+
+
+n =int(input())
+for _ in range(n):
+    a = input()
+    d = a.split()
+print(d.concat())
+
+w = "to be or not to be, that is the question"
+l = w.split()
+d = {}
+for item in l:
+    item = item.strip(',')
+    if item in d.keys():
+        d[item] += 1
+    else:
+        d[item] = 1
+print(d)
+    
+
+
+
+
+d = a.split()
+c = d.replace(",","")
+print(len(c))
+
+
